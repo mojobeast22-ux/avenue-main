b3e8c6769f9ef9cccfbc652908b5610c4f548bdf
mojobeast86-tech <mojobeast86@gmail.com>
2026-05-05 12:41:41 +0300

    Remove google font dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import { LanguageProvider } from "@/contexts/LanguageContext"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "أفنيو للأستثمار والتطوير العقاري - Avenue Real Estate",
   description: "شركة استثمارية رائدة تهدف إلى بناء مستقبل أفضل من خلال استثماراتها المتنوعة والمدروسة",
   keywords: "استثمار, عقارات, شراكات, أفنيو للأستثمار والتطوير العقاري, بغداد العراق",
-  icons:"/Avenue Logo Colored.png",
+  icons: "/Avenue Logo Colored.png",
 }
 
 export default function RootLayout({
@@ -20,9 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <LanguageProvider>
-      <body className={inter.className}>{children}</body>
-      </LanguageProvider>
+      <body>
+        <LanguageProvider>{children}</LanguageProvider>
+      </body>
     </html>
   )
 }
